e96e426e9d12ada29b41372c2121639c84c9dfe5
soupbab <jungu1106@naver.com>
2021-08-17 18:24:50 +0900

    2021.08.17 problem solving

--- 0816~0817/1756 피자 굽기.py
+++ 0816~0817/1756 피자 굽기.py
@@ -1,32 +1,47 @@
-## 좋은 풀이 : https://rhdtka21.tistory.com/129
+# 참고 사이트 : https://rhdtka21.tistory.com/129
 
 
-def binary_search(start, end, target, array):
-    idx = -1
+# def binary_search(start, end, target, array):
+#     idx = -1
 
-    while start <= end:
-        mid = (start + end) // 2
+#     while start <= end:
+#         mid = (start + end) // 2
 
-        if array[mid] >= target:
-            idx = mid
-            start = mid + 1
-        else:
-            end = mid - 1
+#         if array[mid] >= target:
+#             idx = mid
+#             start = mid + 1
+#         else:
+#             end = mid - 1
 
-    return idx + 1
+#     return idx + 1
 
 
 def main():
     for i in range(1, d):
         oven[i] = min(oven[i], oven[i-1])
+    oven.reverse()
 
-    bottom = len(oven)
+    # bottom = len(oven)
 
-    for pd in dough:
-        depth = binary_search(0, bottom-1, pd, oven[:bottom])
-        bottom = depth - 1
+    # for pd in dough:
+    #     depth = binary_search(0, bottom-1, pd, oven[:bottom])
+    #     bottom = depth - 1
 
-    print(depth)
+    # print(depth)
+
+    dough_idx = 0
+    depth = 0
+    for i in range(d):
+        if dough_idx > n-1:
+            print(d - (depth - 1))
+            return
+
+        if dough[dough_idx] <= oven[depth]:
+            dough_idx += 1
+
+        depth += 1
+        
+    print(0)
 
 
 if __name__ == "__main__":
